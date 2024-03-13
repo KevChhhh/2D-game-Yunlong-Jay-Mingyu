2eddd2a8d3d40c8abdb706a868a9234766180545
JayMCook <jmcook5520@gmail.com>
2024-03-13 13:09:42 -0400

    Death Test

--- Node2D.gd
+++ Node2D.gd
@@ -1,15 +1,19 @@
-extends PathFollow2D
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+var activated = false
+
+
+
+func _on_area_2d_body_entered(body):
+	if !activated:
+		body.respawn_point = self
+		activated = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	progress_ratio += .01
 	pass
-
-
-	
